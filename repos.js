d1fa7ee8cc9cf960c22ad818b488ffe14d27ac13
ewanpalmertd <ejp.wrk@gmail.com>
2024-12-31 16:04:03 +0000

    added personal repo list

--- repos.js
+++ repos.js
@@ -1,15 +1,20 @@
 const username = "ewanpalmertd";
 const repoList = document.getElementById("repo-list");
+const allowedRepos = ["usdlink", "website", "UsdShadeMtlx"];
 
 // fetch repos
 fetch(`https://api.github.com/users/${username}/repos`)
   .then((response) => response.json())
   .then((data) => {
     data.forEach((repo) => {
-      // create a list item for each repo
-      const listItem = document.createElement("li");
-      listItem.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a>`;
-      repoList.appendChild(listItem);
+      if (allowedRepos.includes(repo.name) && repo.name !== "ewanpalmertd") {
+        // create a list item for each repo
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<a href="${repo.html_url}" target="_blank">
+          <i class="fas fa-code-branch"></i> ${repo.name}
+        </a>`;
+        repoList.appendChild(listItem);
+      }
     });
   })
   .catch((error) => console.error("Error fetching repositories:", error));
